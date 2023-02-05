7c7c7b4566cd334e532c5a05e35a08dc210f3e77
Gonzalo <gonzabarreiro7@gmail.com>
2023-02-05 14:42:36 -0300

    Entregable 16

--- app.js
+++ app.js
@@ -115,6 +115,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const compression = require('compression');
+app.use(compression());
 const path = require('path')
 
 //configuramos mongoStore
@@ -127,7 +129,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, './views')));
 
 app.use(cookieParser());
 app.use(session({
@@ -143,23 +145,40 @@ app.use(session({
 
 //FUNCIÓN DE MIDDLEWARE --> recibe 3 parámetros
 const auth = (req, res, next) => {
-    if (req.session.nombre == 'Juan') {
+    if (req.session.nombre === 'Gon') {
         return next();
     }
     return res. status(401).send('Acceso no autorizado.')
 };
 
-app.post('/login', (req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'))
+})
+
+app.get('/nombre', (req, res) => {
     const nombre = req.query.nombre;
     if (!nombre) {
-        return res.status(400).send('Nombre no especificado');
+        return res.status(200).send('Nombre no especificado');
     }
 
-    if(req.session.contador) {
-        req.session.contador++;
-        res.sendFile(path.join(__dirname + "/public/index.html"))
-        // res.send(
-        //     `<link="stylesheet" href="/views/main.css"><div style="background-color: red;"> Hola ${nombre} has iniciado sesión ${req.session.contador} veces</div>`);
+    // if (nombre !== 'Gon') {
+    //     return res.send('Nombre incorrecto')
+    // }
+
+    if (nombre !== 'Gon') {
+        res.sendFile(path.join(__dirname + "/views(signup.html"));
+    }
+
+    if (nombre === 'Gon') {
+        res.sendFile(path.join(__dirname + "/public"))
+    
+
+    // if(req.session.contador) {
+    //     req.session.contador++;
+    //     // res.send(
+    //     //     `<div style="background-color: red;"> Hola ${nombre} has iniciado sesión ${req.session.contador} veces</div>`);
+    //     res.sendFile(path.join(__dirname + "/views/signup.html"));
+
     } else {
         req.session.nombre = nombre;
         req.session.contador = 1;
@@ -198,13 +217,20 @@ app.get('/olvidar', (req, res) => {
     });
 });
 
-app.get('/logout', () => {
+app.get('/logout', (req, res) => {
     req.session.destroy((err) => {
         if(!err) {
-            return res.send('LogOut realizado correctamente');
+            return res.send('Log Out realizado correctamente');
         }
         res.status(500).send('Ha ocurrido un error durante el logout')
     });
+});
+
+app.post('/auth', (req, res) => {
+    const nombre = req.body;
+    if (nombre === 'Gon') {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+    }
 });
 
 const port = 8080;
